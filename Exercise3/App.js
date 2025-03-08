2fb152b9b206784fabb3987be14f9e6c55e27dd1
tfozo <abenezermuluneh9@gmail.com>
2025-03-07 23:46:35 -0700

    lab06 done

---
+++ Exercise3/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import EngineeringTopics from './EngineeringTopics';
+
+function App() {
+  return (
+    <div className="App">
+      <EngineeringTopics />
+    </div>
+  );
+}
+
+export default App;
